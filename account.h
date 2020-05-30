a93fb4437b2241d299768ef08b073fff164ff282
omer-re <1EkrV5mY>
2020-05-30 20:53:09 +0300

    Merge branch 'master' of C:\Users\omer.reuveni\CLionProjects\046209_HW2 with conflicts.

--- account.h
+++ account.h
@@ -14,7 +14,7 @@
 #include <cstdio>
 #include <stdlib.h>
 
-
+using namespace std;
 //********************************************
 // Class name:  account
 // Description: used for maintaining the accounts in the bank's system.
@@ -24,44 +24,41 @@
 //**************************************************************************************
 class account {
 private:
-	const unsigned int _account_id;
-	unsigned int _balance;
-	unsigned int _num_of_Readers;  // used for readers-writers implementation
+    const unsigned int _account_id;
+    unsigned int _balance;
+    unsigned int _num_of_Readers;  // used for readers-writers implementation
     const int _password;
 
-	pthread_mutex_t readlock, writelock; // for readers/writers lock implementation
-
+    pthread_mutex_t readlock, writelock; // for readers/writers lock implementation
+    unsigned int _num_of_Readers;  // used for readers-writers implementation
 
 public:
-	// C'tor + init the object's mutexs
-    account(unsigned int acntNum, int initBalance, std::string pass, std::string atmID) :
+    // C'tor + init the object's mutexs
+    account(unsigned int acntNum, int initBalance, string pass, string atmID) :
             _account_id(acntNum), _balance(initBalance), _password(pass), _num_of_Readers(0) {
+        if (pthread_mutex_init(&readlock, NULL) ||
+            pthread_mutex_init(&writelock, NULL)) {   // init allocates memory, need to make sure sys call didnt fail
+            perror("system call error:");
+            exit(1);
+        }
+    }
 
-		if (pthread_mutex_init(&readlock, NULL) ||
-			pthread_mutex_init(&writelock, NULL)) {   // init allocates memory, need to make sure sys call didnt fail
-			perror("system call error:");
-			exit(1);
-		}
-	}
+    //D'tor + destroy locks
+    ~account() {
+        pthread_mutex_destroy(&readlock);
+        pthread_mutex_destroy(&writelock);
+    }
 
-	//D'tor + destroy locks
-	~account() {
-		pthread_mutex_destroy(&readlock);
-		pthread_mutex_destroy(&writelock);
-	}
-
-	void deposit(unsigned int amount_of_money);
-	bool withdrawal(unsigned int amount_of_money);  // if there's not enough balance - return false
-	unsigned int getBalance();
-
+    void deposit(unsigned int amount_of_money);
+    bool withdrawal(unsigned int amount_of_money);  // if there's not enough balance - return false
+    unsigned int getBalance();
     bool check_password(unsigned int password);
-	void lock(std::string rw); // Wrapper function for managing Readers/Writers mutual exclusions
-	void unlock(std::string rw); // Wrapper function for managing Readers/Writers mutual exclusions
-	void account_print();
+    void lock(std::string rw); // Wrapper function for managing Readers/Writers mutual exclusions
+    void unlock(std::string rw); // Wrapper function for managing Readers/Writers mutual exclusions
+    void account_print();
 
-	int check_num_of_readers();
+    int check_num_of_readers();
 };
-
 
 
 #endif //HW2_ACCOUNT_H
